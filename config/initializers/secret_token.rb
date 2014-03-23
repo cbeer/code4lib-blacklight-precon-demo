f7b34ed5c73b4daf6ff78d874c54a1662133e33d
Chris Beer <cabeer@stanford.edu>
2014-03-23 10:11:35 -0700

    initial commit after running rails new with the blacklight application template

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SearchApp::Application.config.secret_key_base = 'ad403c9bdd3842abe0b42978cedc46f8c088c2558125a621d552edfc7ad2ff8179d30c641f3016d3e51b6c6b68774dddbf588228be929be9a7a8cf67e099480e'
