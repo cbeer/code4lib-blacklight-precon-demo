e1026c50790845568ad780d8f05b5da0d20c2bff
Chris Beer <cabeer@stanford.edu>
2014-03-23 19:13:51 -0700

    example of helper_method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def render_dimension_values data = {}
+    h = Hash[data[:value].scan(/\w+:/).zip(data[:value].split(/\w+:/).reject { |x| x.blank? })]
+    
+    safe_join(h.map do |k,v|
+      content_tag :div do
+        (content_tag :strong, k) + 
+        (content_tag :span, v)
+      end
+    end, "")
+  end
 end
